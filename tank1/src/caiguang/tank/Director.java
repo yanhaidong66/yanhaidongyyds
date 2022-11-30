736e6022d7142ba6ed3f511b40367ba313b698a3
haidong <1178258121@qq.com>
2022-11-30 12:32:53 +0800

    have create welcomeScene

--- tank1/src/caiguang/tank/Director.java
+++ tank1/src/caiguang/tank/Director.java
@@ -1,5 +1,6 @@
 package caiguang.tank;
 
+import caiguang.tank.scene.Index;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -10,6 +11,8 @@ public class Director {
 
     public static final double  WIDTH = 960,HEIGHT =640;
 
+    private Stage stage;
+
     private Director(){};
 
     private static Director instance=new Director();
@@ -19,12 +22,27 @@ public class Director {
     }
 
     public void init(Stage stage){
+        this.stage=stage;
         AnchorPane root=new AnchorPane();
         Scene scene=new Scene(root,WIDTH,HEIGHT);
         stage.setTitle("坦克");
-        stage.getIcons().add(new Image("file:image/logo.png"));
+        stage.getIcons().add(new Image("image/logo.png"));
         stage.setResizable(false);//改变窗口大小
         stage.setScene(scene);
+        toIndex();
+        stage.show();
+    }
+
+    public void gameStart(){
+
+    }
+
+    public void gameOver(){
+
+    }
+    public void toIndex(){
+        Index.load(stage);
+
     }
 
 
--- tank1/src/caiguang/tank/Main.java
+++ tank1/src/caiguang/tank/Main.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 public class Main extends Application{
     @Override
     public void start(Stage stage) throws Exception {
-
+        Director.getInstance().init(stage);
     }
 
     public static void main(String[] args) {
